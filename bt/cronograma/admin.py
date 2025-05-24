6e0fcddab75303a5aaba55833ac39d3b8e5fb973
LuisParada7 <luisdanielparadamesa@gmail.com>
2025-05-23 22:21:01 -0500

    Add core models and integrate import-export

--- bt/cronograma/admin.py
+++ bt/cronograma/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import TrainingType, TrainingAppointment
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+
+@admin.register(TrainingType)
+class TrainingTypeAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
+    ordering = ['name']
+
+@admin.register(TrainingAppointment)
+class TrainingAppointmentAdmin(ImportExportModelAdmin):
+    list_display = ('user', 'date', 'time', 'location', 'display_training_types', 'phone', 'notes', 'completed',)
+    list_filter = ('date', 'time', 'completed', 'user', 'training_type',)
+    search_fields = ('user__username', 'user__email', 'date', 'location', 'phone', 'training_type__name')
+    ordering = ['-date', '-time']
+
+    def display_training_types(self, obj):
+        """
+        Crea una cadena con los nombres de los TrainingType asociados.
+        """
+        return ", ".join([tt.name for tt in obj.training_type.all()])
+    display_training_types.short_description = "Tipos de Entrenamiento" # Nombre de la columna en el admin
+
+    # Organizar los campos en el formulario de creación/edición
+    fieldsets = (
+        (None, { # Primera sección, sin título
+            'fields': ('user', 'date', 'time', 'location')
+        }),
+        ('Detalles del Entrenamiento', {
+            'fields': ('training_type', 'phone', 'notes') # training_type es M2M, se mostrará con el widget apropiado
+        }),
+        ('Estado', {
+            'fields': ('completed',)
+        }),
+    )
--- bt/cronograma/models.py
+++ bt/cronograma/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class TrainingType(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Nombre")
+    description = models.CharField(max_length=50, verbose_name="Descripción")
+
+    class Meta:
+        verbose_name = "Tipo de entrenamiento"
+        verbose_name_plural = "Tipos de entrenamiento"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class TrainingAppointment(models.Model):
+    date = models.DateField(verbose_name="Fecha", default=timezone.now)
+    time = models.TimeField(verbose_name="Hora del entrenamiento")
+    location = models.CharField(max_length=50, verbose_name="Dirección lugar de entrenamiento")
+    training_type = models.ManyToManyField('TrainingType')
+    phone = models.CharField(max_length=20, verbose_name="Celular del cliente")
+    notes = models.CharField(max_length=150, verbose_name="Comentarios adicionales")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Usuario", default=None)
+    completed = models.BooleanField(default=False, verbose_name="Completado")
+
+    class Meta:
+        verbose_name = "Sesión de entrenamiento"
+        verbose_name_plural = "Sesiones de entrenamiento"
+        ordering = ['date']
+
+    def __str__(self):
+        return f'Cita para {self.user.username} el {self.date} a las {self.time.strftime("%H:%M")}'
