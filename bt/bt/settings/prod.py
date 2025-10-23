95e566f3822afd363e72d664a73ae434d117430e
LuisParada7 <luisdanielparadamesa@gmail.com>
2025-10-22 23:14:53 -0500

    Fix error 404 not found vercel 5

--- bt/bt/settings/prod.py
+++ bt/bt/settings/prod.py
@@ -1,7 +1,7 @@
 import os
 from .base import *
 
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = "django-insecure-vq)gqq*y-(u3!zv=3c21ko0mt6k73-kmy&m_y(xip6j4z&mj9-"
 DEBUG = False
 ALLOWED_HOSTS = ['.vercel.app']
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
